e3d51dbd63464a4eaee3fb2dc31f7dce62312f5d
Nusrat Jannat <72993483+nusratjannat-2001@users.noreply.github.com>
2023-08-24 22:04:00 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,265 +1,110 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(CalculatorApp());
+}
 
-class MyApp extends StatelessWidget {
+class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Calculator(),
+      title: 'Calculator App',
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: CalculatorScreen(),
     );
   }
 }
 
-class Calculator extends StatefulWidget {
-  @override
-  _CalculatorState createState() => _CalculatorState();
-}
-
-class _CalculatorState extends State<Calculator> {
-  dynamic displaytxt = 20;
-  //Button Widget
-  Widget calcbutton(String btntxt, Color btncolor, Color txtcolor) {
-    return Container(
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          padding: EdgeInsets.all(20),
-          shape: CircleBorder(),
-          backgroundColor: btncolor,
-        ),
-        onPressed: () {
-          calculation(btntxt);
-        },
-        child: Text(
-          btntxt,
-          style: TextStyle(
-            fontSize: 35,
-            color: txtcolor,
-          ),
-        ),
-      ),
-    );
-  }
-
+class CalculatorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //Calculator
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text('Calculator'),
-        backgroundColor: Colors.black,
-      ),
+      appBar: AppBar(title: Text('My First Calculator')),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 5),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            // Calculator display
-            SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      '$text',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 100,
-                      ),
-                    ),
-                  )
-                ],
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              'Input field', // Display the ongoing calculation or result here
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
               ),
-            ),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold ,),
+              textAlign: TextAlign.end,
+              readOnly: true,
+              controller: TextEditingController(text: 'give me input'), // Replace '579' with your current input
+            )
+            ,
+            SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                calcbutton('AC', Colors.grey, Colors.black),
-                calcbutton('+/-', Colors.grey, Colors.black),
-                calcbutton('%', Colors.grey, Colors.black),
-                calcbutton('/', Colors.amber[700]!, Colors.white),
+              children: [
+                CalculatorButton(text: '7'),
+                CalculatorButton(text: '8'),
+                CalculatorButton(text: '9'),
+                CalculatorButton(text: '÷'),
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
+            SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                calcbutton('7', Colors.grey[850]!, Colors.white),
-                calcbutton('8', Colors.grey[850]!, Colors.white),
-                calcbutton('9', Colors.grey[850]!, Colors.white),
-                calcbutton('x', Colors.amber[700]!, Colors.white),
+              children: [
+                CalculatorButton(text: '4'),
+                CalculatorButton(text: '5'),
+                CalculatorButton(text: '6'),
+                CalculatorButton(text: '×'),
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
+            SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                calcbutton('4', Colors.grey[850]!, Colors.white),
-                calcbutton('5', Colors.grey[850]!, Colors.white),
-                calcbutton('6', Colors.grey[850]!, Colors.white),
-                calcbutton('-', Colors.amber[700]!, Colors.white),
+              children: [
+                CalculatorButton(text: '1'),
+                CalculatorButton(text: '2'),
+                CalculatorButton(text: '3'),
+                CalculatorButton(text: '-'),
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
+            SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                calcbutton('1', Colors.grey[850]!, Colors.white),
-                calcbutton('2', Colors.grey[850]!, Colors.white),
-                calcbutton('3', Colors.grey[850]!, Colors.white),
-                calcbutton('+', Colors.amber[700]!, Colors.white),
+              children: [
+                CalculatorButton(text: '0'),
+                CalculatorButton(text: '.'),
+                CalculatorButton(text: '='),
+                CalculatorButton(text: '+'),
               ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                //this is button Zero
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.fromLTRB(34, 20, 128, 20),
-                    shape: StadiumBorder(),
-                    backgroundColor: Colors.grey[850],
-                  ),
-                  onPressed: () {
-                    calculation('0');
-                  },
-                  child: Text(
-                    '0',
-                    style: TextStyle(fontSize: 35, color: Colors.white),
-                  ),
-                ),
-                calcbutton('.', Colors.grey[850]!, Colors.white),
-                calcbutton('=', Colors.white, Colors.amber[700]!),
-              ],
-            ),
-            SizedBox(
-              height: 10,
             ),
           ],
         ),
       ),
     );
   }
+}
 
-  //Calculator logic
-  dynamic text = '0';
-  double numOne = 0;
-  double numTwo = 0;
+class CalculatorButton extends StatelessWidget {
+  final String text;
 
-  dynamic result = '';
-  dynamic finalResult = '';
-  dynamic opr = '';
-  dynamic preOpr = '';
-  void calculation(btnText) {
-    if (btnText == 'AC') {
-      text = '0';
-      numOne = 0;
-      numTwo = 0;
-      result = '';
-      finalResult = '0';
-      opr = '';
-      preOpr = '';
-    } else if (opr == '=' && btnText == '=') {
-      if (preOpr == '+') {
-        finalResult = add();
-      } else if (preOpr == '-') {
-        finalResult = sub();
-      } else if (preOpr == 'x') {
-        finalResult = mul();
-      } else if (preOpr == '/') {
-        finalResult = div();
-      }
-    } else if (btnText == '+' ||
-        btnText == '-' ||
-        btnText == 'x' ||
-        btnText == '/' ||
-        btnText == '=') {
-      if (numOne == 0) {
-        numOne = double.parse(result);
-      } else {
-        numTwo = double.parse(result);
-      }
+  const CalculatorButton({required this.text});
 
-      if (opr == '+') {
-        finalResult = add();
-      } else if (opr == '-') {
-        finalResult = sub();
-      } else if (opr == 'x') {
-        finalResult = mul();
-      } else if (opr == '/') {
-        finalResult = div();
-      }
-      preOpr = opr;
-      opr = btnText;
-      result = '';
-    } else if (btnText == '%') {
-      result = numOne / 100;
-      finalResult = doesContainDecimal(result);
-    } else if (btnText == '.') {
-      if (!result.toString().contains('.')) {
-        result = '$result.';
-      }
-      finalResult = result;
-    } else if (btnText == '+/-') {
-      result.toString().startsWith('-')
-          ? result = result.toString().substring(1)
-          : result = '-$result';
-      finalResult = result;
-    } else {
-      result = result + btnText;
-      finalResult = result;
-    }
-
-    setState(() {
-      text = finalResult;
-    });
-  }
-
-  String add() {
-    result = (numOne + numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String sub() {
-    result = (numOne - numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String mul() {
-    result = (numOne * numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String div() {
-    result = (numOne / numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String doesContainDecimal(dynamic result) {
-    if (result.toString().contains('.')) {
-      List<String> splitDecimal = result.toString().split('.');
-      if (!(int.parse(splitDecimal[1]) > 0)) {
-        return result = splitDecimal[0].toString();
-      }
-    }
-    return result;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 80,
+      height: 80,
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
